c9613a46b1f38f09c14f473398c8b34247f48496
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-23 18:39:55 -0700

    Update monitor.sv

--- monitor.sv
+++ monitor.sv
@@ -1,33 +1,35 @@
 `timescale 1ns/1ps
+
 `include "transaction_class.sv"
 
 class monitor_class;
-    virtual main_if test intf;
-    transaction_class packet;
-    mailbox mon2scb;
+  virtual main_if vif;
+  transaction_class trans_obj;
+  mailbox mon2scb;
 
-    function new(mailbox mon2scb, input virtual main_if test intf);
-        this.mon2scb = mon2scb;
-        this.intf = intf;
-        packet = new();
-    endfunction
+  function new(mailbox mon2scb, input virtual main_if vif);
+    this.mon2scb = mon2scb;
+    this.vif = vif;
+    trans_obj = new();
+  endfunction
 
-    task run();
-        forever begin
-            transaction_class trans = new();
-            @(posedge intf.clk);
-            trans.BestDist = intf.cb.BestDist;  
-            trans.motionX = intf.cb.motionX; 
-            trans.motionY = intf.cb.motionY; 
-            trans.completed = intf.cb.completed; 
-            trans.AddressR = intf.cb.AddressR; 
-            trans.AddressS1 = intf.cb.AddressS1;
-            trans.AddressS2 = intf.cb.AddressS2;
-            mon2scb.put(trans);
-        end
-    endtask
+  task run();
+    forever begin
+      transaction_class trans;
+      @(posedge vif.clk);
+      trans_obj = new();
+      trans_obj.BestDist = vif.BestDist;
+      trans_obj.motionX = vif.motionX;
+      trans_obj.motionY = vif.motionY;
+      trans_obj.completed = vif.completed;
+      trans_obj.AddressR = vif.AddressR;
+      trans_obj.AddressS1 = vif.AddressS1;
+      trans_obj.AddressS2 = vif.AddressS2;
+      mon2scb.put(trans_obj);
+    end
+  endtask
 
-    task wrap_up();
-        // Empty for now
-    endtask
+  task wrap_up();
+    // empty for now
+  endtask
 endclass
