9c7c92936e66464ed3efd3ea3a7539f7a75ecbc6
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-23 17:40:06 -0700

    Create test.sv

---
+++ test.sv
@@ -0,0 +1,18 @@
+`timescale 1ns/1ps
+
+`include "environment.sv"
+
+program testbench(main_if main_interface); 
+
+  // Instance of the environment class
+  test_environment test_env;
+  
+  // Initial block to set up and run the environment
+  initial begin
+    test_env = new(main_interface);  // Create a new environment instance with the given interface
+    test_env.gen_inst.trans_count = `TRANSACTION_COUNT;  // Set the total number of transactions to be generated
+    test_env.build();  // Build the environment
+    test_env.run();  // Start the run task of the environment
+    test_env.wrap_up();  // Wrap up the environment
+  end
+endprogram
