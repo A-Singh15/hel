e68932080fe3457b6beb69325e32cd55b28e7409
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-23 18:21:16 -0700

    Update test.sv

--- test.sv
+++ test.sv
@@ -2,17 +2,15 @@
 
 `include "environment.sv"
 
-program testbench(main_if main_interface); 
+program test_program(main_if test_if); 
 
   // Instance of the environment class
   environment env;
   
   // Initial block to set up and run the environment
   initial begin
-    env = new(main_interface);  // Create a new environment instance with the given interface
-    env.gen_inst.trans_count = `TRANSACTION_COUNT;  // Set the total number of transactions to be generated
-    env.build();  // Build the environment
-    env.run();  // Start the run task of the environment
-    env.wrap_up();  // Wrap up the environment
+    env = new(test_if);  // Create a new environment instance with the given interface
+    env.generator_inst.trans_count = `TRANSACTION_COUNT;  // Set the total number of transactions to be generated
+    env.run_env();  // Start the run task of the environment
   end
 endprogram
