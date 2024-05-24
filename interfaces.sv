c6308214f826cdff687b7176b92920fcfbce40a2
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-23 18:13:40 -0700

    Update interfaces.sv

--- interfaces.sv
+++ interfaces.sv
@@ -1,102 +1,92 @@
-
 `timescale 1ns/1ps
 
-// General Interface
+// Main interface declaration
 interface main_if(input bit clk);
+    logic start;
+    logic completed;
+    logic [7:0] BestDist;
+    logic [3:0] motionX, motionY;
+    logic [7:0] R, S1, S2;
+    logic [7:0] AddressR;
+    logic [9:0] AddressS1, AddressS2;
 
-  // Signals
-  logic init_signal;
-  logic complete_signal;
-  logic [7:0] distance_value;
-  logic [3:0] x_motion, y_motion;
-  logic [7:0] R_val, S1_val, S2_val;
-  logic [7:0] addr_R;
-  logic [9:0] addr_S1, addr_S2;
+    // Driver clocking block
+    clocking cb @(posedge clk);
+        default input #1 output #1;
+        output R;
+        output S1;
+        output S2;
+        output start;
+        input BestDist;
+        input motionX;
+        input motionY;
+        input completed;
+        input AddressR;
+        input AddressS1;
+        input AddressS2;
+    endclocking
 
-  // Driver clocking block
-  clocking driver_cb @(posedge clk);
-    default input #1 output #1;
-    output R_val;
-    output S1_val;
-    output S2_val;
-    output init_signal;
-    input distance_value;   
-    input x_motion;  
-    input y_motion;  
-    input complete_signal;  
-    input addr_R;  
-    input addr_S1;
-    input addr_S2;  
-  endclocking
-
-  // Modport for driver
-  modport drv (clocking driver_cb, input clk);
-
+    // Modport for driving signals
+    modport test (clocking cb, input clk);
 endinterface : main_if
-
-
-`default_nettype none
 
 // Memory Estimator Interface
 interface mem_est_if(input bit clk);
+    bit start;
+    logic [3:0] motionX;
+    logic [3:0] motionY;
+    integer Expected_motionX;
+    integer Expected_motionY;
+    logic [7:0] AddressR;
+    logic [9:0] AddressS1;
+    logic [9:0] AddressS2;
+    logic [7:0] R;
+    logic [7:0] S1;
+    logic [7:0] S2;
+    logic [7:0] BestDist;
+    logic completed;
+    logic [7:0] R_mem[`RMEM_MAX-1:0];
+    logic [7:0] S_mem[`SMEM_MAX-1:0];
 
-  // Signals
-  logic start_signal; 
-  logic [3:0] x_vector;
-  logic [3:0] y_vector;
-  integer exp_x_vector;
-  integer exp_y_vector;
-  logic [7:0] addr_R;
-  logic [9:0] addr_S1;
-  logic [9:0] addr_S2;
-  logic [7:0] R_mem_val;
-  logic [7:0] S1_mem_val;
-  logic [7:0] S2_mem_val;
-  logic [7:0] best_distance;
-  logic comp_signal;
-  logic [7:0] R_mem_data[`RMEM_MAX-1:0];
-  logic [7:0] S_mem_data[`SMEM_MAX-1:0];
+    // Clocking block for driver
+    clocking ME_driver_cb @(posedge clk);
+        default input #1 output #1;
+        output R_mem;
+        output S_mem;
+        output R;
+        output S1;
+        output S2;
+        output Expected_motionX;
+        output Expected_motionY;
+        input BestDist;
+        input motionX, motionY;
+        input AddressR;
+        input AddressS1;
+        input AddressS2;
+        input completed;
+    endclocking
 
-  // Driver clocking block
-  clocking driver_cb @(posedge clk);
-    default input #1 output #1;
-    output R_mem_data;
-    output S_mem_data;
-    output R_mem_val;
-    output S1_mem_val;
-    output S2_mem_val;
-    output exp_x_vector;
-    output exp_y_vector;
-    input best_distance;
-    input x_vector, y_vector;
-    input addr_R;
-    input addr_S1;
-    input addr_S2;
-    input comp_signal;
-  endclocking
-  
-  // Monitor clocking block
-  clocking monitor_cb @(posedge clk);
-    default input #1 output #1;
-    input R_mem_data;
-    input S_mem_data;
-    input R_mem_val;
-    input S1_mem_val;
-    input S2_mem_val;
-    input exp_x_vector;
-    input exp_y_vector;
-    input x_vector, y_vector;
-    input addr_R;
-    input addr_S1;
-    input addr_S2;
-    input comp_signal;
-    input best_distance;
-  endclocking
-  
-  // Modport for driver
-  modport DRIVER (clocking driver_cb, input clk, start_signal);
-  
-  // Modport for monitor
-  modport MONITOR (clocking monitor_cb, input clk, start_signal);
+    // Clocking block for monitor
+    clocking ME_monitor_cb @(posedge clk);
+        default input #1 output #1;
+        input R_mem;
+        input S_mem;
+        input R;
+        input S1;
+        input S2;
+        input Expected_motionX;
+        input Expected_motionY;
+        input motionX, motionY;
+        input AddressR;
+        input AddressS1;
+        input AddressS2;
+        input completed;
+        input BestDist;
+    endclocking
 
+    // Modport for driver
+    modport ME_DRIVER (clocking ME_driver_cb, input clk, start);
+
+    // Modport for monitor
+    modport ME_MONITOR (clocking ME_monitor_cb, input clk, start);
 endinterface
