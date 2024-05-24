007294d7a33c0963a2c3fc63354dceea36160a4b
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-23 18:39:39 -0700

    Update driver.sv

--- driver.sv
+++ driver.sv
@@ -1,47 +1,51 @@
 `timescale 1ns/1ps
+
 `include "transaction_class.sv"
 
 class driver_class;
-    int num_transactions = 0;
-    virtual main_if test intf;
-    transaction_class trans_obj;
-    mailbox gen2drv, drv2gen, drv2scb;
+  int num_transactions = 0;
+  virtual main_if vif;
+  transaction_class trans_obj;
+  mailbox gen2drv, drv2gen, drv2scb;
 
-    function new(mailbox gen2drv, drv2gen, drv2scb, input virtual main_if test intf);
-        this.gen2drv = gen2drv;
-        this.drv2gen = drv2gen;
-        this.drv2scb = drv2scb;
-        this.intf = intf;
-        trans_obj = new();
-    endfunction
+  function new(mailbox gen2drv, drv2gen, drv2scb, input virtual main_if vif);
+    this.gen2drv = gen2drv;
+    this.drv2gen = drv2gen;
+    this.drv2scb = drv2scb;
+    this.vif = vif;
+    trans_obj = new();
+  endfunction
 
-    task reset();
-        $display("********* [DRIVER] Reset Started *********");
-        intf.cb.start <= 0;
-        intf.cb.R <= 0;
-        intf.cb.S1 <= 0;
-        intf.cb.S2 <= 0;
-        $display("********* [DRIVER] Reset Ended *********");
-    endtask
+  task reset();
+    $display("--------- [DRIVER] Reset Started ---------");
+    vif.start <= 0;
+    vif.R <= 0;
+    vif.S1 <= 0;
+    vif.S2 <= 0;
+    $display("--------- [DRIVER] Reset Ended ---------");
+  endtask
 
-    task run();
-        forever begin
-            transaction_class trans;
-            @(posedge intf.clk);
-            gen2drv.get(trans);
-            intf.cb.start <= trans.start;
-            intf.cb.R <= trans.R;
-            intf.cb.S1 <= trans.S1;
-            intf.cb.S2 <= trans.S2;
-            drv2scb.put(trans);
-            num_transactions++;
-        end
-    endtask
+  task run();
+    forever begin
+      transaction_class trans;
+      @(posedge vif.clk);
+      gen2drv.get(trans);
+      vif.start <= trans.start;
+      vif.R <= trans.R;
+      vif.S1 <= trans.S1;
+      vif.S2 <= trans.S2;
+      drv2scb.put(trans);
+      num_transactions++;
+    end
+  endtask
 
-    task wrap_up();
-        wait(intf.cb.BestDist == 1);
-        @(posedge intf.clk);
-        $display("******** Simulation Finished ********");
-        $finish;
-    endtask
+  task wrap_up();
+    wait (vif.BestDist == 1);
+    @vif;
+    $display("------------------------------------------------------------------");
+    $display("********  best distance = 1;   **********");
+    $display("*****  Finishing simulation  **********");
+    $display("------------------------------------------------------------------");
+    $finish;
+  endtask
 endclass
