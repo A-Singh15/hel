3e82ad94435340ffc6a4e50b408e5dad683b4b3f
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-23 18:36:55 -0700

    Update interfaces.sv

--- interfaces.sv
+++ interfaces.sv
@@ -1,45 +1,37 @@
 `timescale 1ns/1ps
 
 interface main_if(input bit clk);
-    // Interface signals
     logic start;
-    logic [7:0] BestDist;
-    logic [3:0] motionX;
-    logic [3:0] motionY;
     logic completed;
+    logic [7:0] BestDist;
+    logic [3:0] motionX, motionY;
+    logic [7:0] R, S1, S2;
     logic [7:0] AddressR;
     logic [9:0] AddressS1, AddressS2;
-    logic [7:0] R, S1, S2;
 
-    // Clocking block for driver
     clocking cb @(posedge clk);
         default input #1 output #1;
-        output start, R, S1, S2;
+        output R, S1, S2, start;
         input BestDist, motionX, motionY, completed, AddressR, AddressS1, AddressS2;
     endclocking
 
-    // Modport for driver
     modport test (clocking cb, input clk);
 endinterface
 
 interface mem_est_if(input bit clk);
-    // Interface signals
     logic start;
+    logic [7:0] R, S1, S2;
     logic [7:0] BestDist;
-    logic [3:0] motionX;
-    logic [3:0] motionY;
+    logic [3:0] motionX, motionY;
     logic completed;
     logic [7:0] AddressR;
     logic [9:0] AddressS1, AddressS2;
-    logic [7:0] R, S1, S2;
 
-    // Clocking block for driver
     clocking cb @(posedge clk);
         default input #1 output #1;
-        output start, R, S1, S2;
+        output R, S1, S2, start;
         input BestDist, motionX, motionY, completed, AddressR, AddressS1, AddressS2;
     endclocking
 
-    // Modport for driver
-    modport DRIVER (clocking cb, input clk);
+    modport mem_est (clocking cb, input clk);
 endinterface
